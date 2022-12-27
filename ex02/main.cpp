b85e6bd87518c4d44210b3db993ff8512d1cb222
Bunyod16 <bunyodshams@gmail.com>
2022-12-27 14:09:12 +0800

    chore: added tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,16 +3,46 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-    Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    Fixed c(10);
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max( a, c ) << std::endl;
-    std::cout << Fixed::min(a, c) << std::endl;
+    // {
+    //     Fixed a(10.0f);
+    //     Fixed b(10);
+
+    //     if (a != b)
+    //         std::cout << "A != B" << std::endl;
+    //     if (a == b)
+    //         std::cout << "A == B" << std::endl;
+    //     if (a > b)
+    //         std::cout << "A > B" << std::endl;
+    //     if (a >= b)
+    //         std::cout << "A >= B" << std::endl;
+    //     if (a < b)
+    //         std::cout << "A < B" << std::endl;
+    //     if (a <= b)
+    //         std::cout << "A <= B" << std::endl;
+    // }
+    // {
+    //     Fixed a(2.5f);
+    //     Fixed b(10);
+
+    //     a = a + b; // a = 12.5f
+    //     b = b - 2.5f; // b = 7.5f
+    //     a = a * b; // a = 93.75
+    //     b = b / b; // b = 1
+    //     std::cout << a << std::endl;
+    //     std::cout << b << std::endl;
+    // }
+    // {    
+    //     Fixed a;
+    //     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    //     Fixed c(10);
+    //     std::cout << a << std::endl;
+    //     std::cout << ++a << std::endl;
+    //     std::cout << a << std::endl;
+    //     std::cout << a++ << std::endl;
+    //     std::cout << a << std::endl;
+    //     std::cout << b << std::endl;
+    //     std::cout << Fixed::max( a, c ) << std::endl;
+    //     std::cout << Fixed::min(a, c) << std::endl;
+    // }
     return 0;
 }
